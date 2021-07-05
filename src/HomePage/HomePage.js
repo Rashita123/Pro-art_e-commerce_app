1c65dcddc6c762ce1b2e425384c1a8ecbab0f8c2
Rashita Mehta <56469693+Rashita123@users.noreply.github.com>
2021-07-05 14:33:46 +0530

    feature: Brands Carousel Added

--- src/HomePage/HomePage.js
+++ src/HomePage/HomePage.js
@@ -2,10 +2,12 @@ import "./HomePage.css";
 import { Hero } from "./Hero/Hero";
 import { Features } from "./Features/Features";
 import { Footer } from "./Footer/Footer";
+import { Carousel } from "./Carousel/Carousel";
 export const HomePage = () => {
   return (
     <div className="home-page">
       <Hero />
+      <Carousel />
       <Features />
       <Footer />
     </div>
