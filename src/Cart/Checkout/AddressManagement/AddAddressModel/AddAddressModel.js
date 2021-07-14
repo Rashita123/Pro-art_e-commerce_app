98a2e4aa3169ec3bb2700606dae2ef3ced2ff15a
Rashita Mehta <56469693+Rashita123@users.noreply.github.com>
2021-07-14 18:52:12 +0530

    refractor: one object for new address

--- src/Cart/Checkout/AddressManagement/AddAddressModel/AddAddressModel.js
+++ src/Cart/Checkout/AddressManagement/AddAddressModel/AddAddressModel.js
@@ -4,47 +4,42 @@ import { Button } from "forkui-lib";
 import { IndianStates } from "./StateDatabases/IndianStates";
 import { USStates } from "./StateDatabases/USStates";
 import { AustralianStates } from "./StateDatabases/AustralianStates";
-import { Link } from "react-router-dom";
 export const AddAddressModel = ({
   setShowAddAddressModel,
   addresses,
   setAddresses
 }) => {
-  const [country, setCountry] = useState("India");
-  const [state, setState] = useState("");
-  const [name, setName] = useState("");
-  const [address, setAddress] = useState("");
-  const [city, setCity] = useState("");
-  const [zip, setZip] = useState(0);
-  const [mob, setMob] = useState(0);
+  const initialAddress = {
+    country: "India",
+    state: null,
+    name: null,
+    address: null,
+    city: null,
+    zip: null,
+    mob: null
+  };
+  const [newAddress, setNewAddress] = useState(initialAddress);
   const updateState = (stateSelected) => {
-    setState(stateSelected);
+    setNewAddress({ ...newAddress, state: stateSelected });
   };
   const submitNewAddress = () => {
-    setAddresses([
-      ...addresses,
-      {
-        id: addresses.length + 1,
-        country,
-        state,
-        name,
-        address,
-        city,
-        zip,
-        mob
-      }
-    ]);
+    setAddresses([...addresses, newAddress]);
+    setShowAddAddressModel(false);
   };
   return (
     <div className="address-management-model">
       <h3>Add New Address</h3>
       <input
-        onChange={(e) => setName(e.target.value)}
+        onChange={(e) => setNewAddress({ ...newAddress, name: e.target.value })}
         type="text"
         placeholder="Enter Name"
       />
       <br />
-      <select onChange={(e) => setCountry(e.target.value)}>
+      <select
+        onChange={(e) =>
+          setNewAddress({ ...newAddress, country: e.target.value })
+        }
+      >
         <option value="India">India</option>
         <option value="Australia">Australia</option>
         <option value="US">US</option>
@@ -52,30 +47,34 @@ export const AddAddressModel = ({
       <br />
 
       <input
-        onChange={(e) => setAddress(e.target.value)}
+        onChange={(e) =>
+          setNewAddress({ ...newAddress, address: e.target.value })
+        }
         type="text"
         placeholder="Enter House No, Street, Colony"
       />
       <br />
       <input
-        onChange={(e) => setCity(e.target.value)}
+        onChange={(e) => setNewAddress({ ...newAddress, city: e.target.value })}
         type="text"
         placeholder="Enter City"
       />
       <br />
-      {country === "India" && <IndianStates updateState={updateState} />}
-      {country === "Australia" && (
+      {newAddress.country === "India" && (
+        <IndianStates updateState={updateState} />
+      )}
+      {newAddress.country === "Australia" && (
         <AustralianStates updateState={updateState} />
       )}
-      {country === "US" && <USStates updateState={updateState} />}
+      {newAddress.country === "US" && <USStates updateState={updateState} />}
       <input
-        onChange={(e) => setZip(e.target.value)}
+        onChange={(e) => setNewAddress({ ...newAddress, zip: e.target.value })}
         type="text"
         placeholder="Enter Zip Code"
       />
       <br />
       <input
-        onChange={(e) => setMob(e.target.value)}
+        onChange={(e) => setNewAddress({ ...newAddress, mob: e.target.value })}
         type="tel"
         pattern="[0-9]{3}-[0-9]{2}-[0-9]{3}"
         placeholder="Enter Mobile Number"
@@ -83,10 +82,7 @@ export const AddAddressModel = ({
       <br />
       <div className="address-model-buttons">
         <Button
-          onClickHandler={() => {
-            submitNewAddress();
-            setShowAddAddressModel(false);
-          }}
+          onClickHandler={submitNewAddress}
           margin="0.5rem"
           text="Submit"
         />
